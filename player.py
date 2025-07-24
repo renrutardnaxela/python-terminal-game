f323abff1abf9a8c75e2b5e3fed5685b2fd0af75
renrutardnaxela <a.c.t.123.at@gmail.com>
2025-07-24 13:13:58 +0100

    changed monster class

---
+++ player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__ (self, player_hp, player_sneak, player_attack, player_defend):
+        self.player_hp = player_hp
+        self.player_sneak = player_sneak
+        self.player_attack = player_attack
+        self.player_defend = player_defend
